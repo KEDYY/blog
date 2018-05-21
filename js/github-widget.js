ba031e8cf6b56de441c4efc0628b181d34b1a26c
張艺飞 <Yifei0727@users.noreply.github.com>
2018-05-21 22:15:55 +0800

    js fix

--- js/github-widget.js
+++ js/github-widget.js
@@ -27,13 +27,12 @@ String.prototype.format = function(args) {
           result = result.replace(reg, args[key]);
         }
       }
-    }
-  } else {
-    for (var i = 0; i < arguments.length; i++) {
-      if (arguments[i] != undefined) {
-        var reg= new RegExp("({)" + i + "(})", "g");
-        result = result.replace(reg, arguments[i]);
-      }
+    } else {
+      for (var i = 0; i < arguments.length; i++) {
+        if (arguments[i] != undefined) {
+          var reg= new RegExp("({)" + i + "(})", "g");
+          result = result.replace(reg, arguments[i]);
+        }
       }
     }
   }
