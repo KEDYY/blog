13789619ab2b6609ed891d980a66da66f99c96a7
張艺飞 <Yifei0727@users.noreply.github.com>
2018-05-23 21:25:59 +0800

    分类 dropdown 优化右侧目录导航

--- js/post-toc.js
+++ js/post-toc.js
@@ -4,35 +4,44 @@
 $(document).ready(function(){
   $('article').find(":header").addClass("section scrollspy");
   $('#markdown-toc').addClass("pinned");
-  record_offset = $('#article-toc').offset().top;
   $('#markdown-toc').prependTo("#article-toc");
   $('#article-recommend').prependTo("#mobile-recommend");
   $('#markdown-toc').children().addClass("section table-of-contents");
   $('.scrollspy').scrollSpy();
   $(window).scroll(scrollToc);
 });
-var record_offset;
 function scrollToc(){
   var scope = $("#article-toc").find($(".pinned"));
   var active = scope.find(".active");
-  if($(window).scrollTop() <= record_offset){
+  if($(window).scrollTop() <= $("#article-user").offset().top + $("#article-user").height()){
     scope.css('top', '');
     return;
   }
   if(active.length ==0){
-    return;
+    if (($(window).scrollTop() - ($('article').height() +$('article').offset().top)) > - $(window).height()*0.3){
+      console.log("yichu1");
+      scope.stop().animate({'top': -scope.height() }, "slow");
+      return;
+    } else if (($(window).scrollTop() - ($('article').height() +$('article').offset().top)) > - $(window).height()*0.8){
+      console.log("yichu2");
+      scope.stop().animate({'top': -scope.height() + $(window).height()/3}, "slow");
+     return;
+    }else{
+      return;
+    }
   }
   var cur = active.offset().top - $(window).scrollTop();
 
-  if ($("#markdown-toc").offset().top - $("#article-user").offset().top  < $("#article-user").height()){
+  if ($("#article-user").offset().top + $("#article-user").height() >= $(window).scrollTop()){
     scope.css('top', '');
+    return;
   }
 
-  if (cur < $(window).height()  * 0.3){
-     scope.animate({'top': $(window).height() *0.5 + scope.offset().top - active.offset().top}, "fast");
+  if (cur < $(window).height()  * 0.35){
+     scope.stop().animate({'top': $(window).height() *0.5 + scope.offset().top - active.offset().top}, "fast");
   }
 
-  if (cur > $(window).height()  * 0.7){
-     scope.animate({'top': $(window).height() *0.5 + scope.offset().top - active.offset().top}, "fast");
+  if (cur > $(window).height()  * 0.65){
+     scope.stop().animate({'top': $(window).height() *0.5 + scope.offset().top - active.offset().top}, "fast");
   }
 }
