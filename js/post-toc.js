9a5356301f110d2bd76321a88c007ff8f6d9f050
張艺飞 <Yifei0727@users.noreply.github.com>
2018-05-26 23:08:16 +0800

    scrollspy config

--- js/post-toc.js
+++ js/post-toc.js
@@ -18,30 +18,28 @@ function scrollToc(){
     return;
   }
   if(active.length ==0){
+
     if (($(window).scrollTop() - ($('article').height() +$('article').offset().top)) > - $(window).height()*0.3){
-      console.log("yichu1");
       scope.stop().animate({'top': -scope.height() }, "slow");
       return;
     } else if (($(window).scrollTop() - ($('article').height() +$('article').offset().top)) > - $(window).height()*0.8){
-      console.log("yichu2");
       scope.stop().animate({'top': -scope.height() + $(window).height()/3}, "slow");
-     return;
-    }else{
+      return;
+    }
+  }else{
+    var cur = active.offset().top - $(window).scrollTop();
+    if (cur < $(window).height()  * 0.23){
+      scope.stop().animate({'top': $(window).height() *0.382 + scope.offset().top - active.offset().top}, "fast");
+      return;
+    }else if (cur > $(window).height()  * 0.5){
+      scope.stop().animate({'top': $(window).height() *0.382 + scope.offset().top - active.offset().top}, "fast");
       return;
     }
   }
-  var cur = active.offset().top - $(window).scrollTop();
 
   if ($("#article-user").offset().top + $("#article-user").height() >= $(window).scrollTop()){
     scope.css('top', '');
     return;
-  }
-
-  if (cur < $(window).height()  * 0.35){
-     scope.stop().animate({'top': $(window).height() *0.5 + scope.offset().top - active.offset().top}, "fast");
-  }
-
-  if (cur > $(window).height()  * 0.65){
-     scope.stop().animate({'top': $(window).height() *0.5 + scope.offset().top - active.offset().top}, "fast");
+  }else{
   }
 }
